db46c3e65f7298907f1dbffd5340b5b115cf0f34
Lucas Secreto <15373552+lsecreto@users.noreply.github.com>
2020-10-23 00:36:58 -0300

    Add default interface and port at server.py

--- server.py
+++ server.py
@@ -5,10 +5,12 @@ from cgiserver import CGIServer
 # Server configuration
 NUM_OF_WORKERS = 4
 ALLOW_CGI = True
+DEFAULT_INTERFACE = '0.0.0.0'
+DEFAULT_PORT = 8080
 
 if __name__ == '__main__':
-    interface = '0.0.0.0'
-    port = 8080
+    interface = DEFAULT_INTERFACE
+    port = DEFAULT_PORT
     if argv[1:]:
         address = argv[1]
         if ':' in address:
@@ -16,10 +18,8 @@ if __name__ == '__main__':
             port = int(address.split(':')[1])
         else:
             port = int(address)
-    address = (interface, port)
-
     if argv[2:]:
         chdir(argv[2])
 
-    with CGIServer(address=address, workers=NUM_OF_WORKERS, allow_cgi=ALLOW_CGI) as server:
+    with CGIServer(address=(interface, port), workers=NUM_OF_WORKERS, allow_cgi=ALLOW_CGI) as server:
         server.start()
